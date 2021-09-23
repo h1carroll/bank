c9e1b5cfa2c2e365bc31524b2abed58fa336ee95
h1carroll <90262864+h1carroll@users.noreply.github.com>
2021-09-23 10:12:18 -0400

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,44 @@
+package com.company;
+public class BankAccount {
+    private double balance;
+    private float interestRate;
+    private int accountID;
+    private static int nextId = 100;
+
+    public BankAccount(){
+        interestRate = 0.02f;
+        accountID = nextId;
+        nextId++;  //same as nextID = nextId + 1
+    }
+    public BankAccount(double initialBalance, float initialRate){
+        balance = initialBalance;
+        interestRate = initialRate;
+        accountID = nextId;
+        nextId += 1;
+    }
+
+    public int getAccountID(){
+        return accountID;
+    }
+    public void deposit(double amount){
+        balance += amount;
+    }
+    public boolean withdraw(double amount){
+        if(amount > balance){
+            return false;
+        }
+        balance = balance - amount;
+        return true;
+    }
+    public double checkBalance(){
+        return balance;
+    }
+    public double addInterest(){
+        balance +=  interestRate*balance;
+        return balance;
+    }
+}
+
+    public void deposit(double amount){
+        balance += amount;
+}
---
+++ src/com/company/Customer.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int customerID;
+    private String name;
+    private ArrayList<BankAccount> accounts;
+
+    public Customer(String customerName, int taxID){
+        customerID = taxID;
+        name = customerName;
+    }
+    public boolean openAccount(double initialDeposit){
+        var newAccount = new BankAccount();
+        newAccount.deposit(initialDeposit);
+        var didSucceed = accounts.add(newAccount);
+        return didSucceed;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
